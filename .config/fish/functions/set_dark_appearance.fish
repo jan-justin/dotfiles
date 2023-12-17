eb1f5b5ec4431760d8fde57aa35a897031407214
Jan-Justin van Tonder <jan.justin.vtonder@gmail.com>
2023-12-17 21:34:32 +0100

    fix indentation of set_{dark,light}_appearance functions

--- .config/fish/functions/set_dark_appearance.fish
+++ .config/fish/functions/set_dark_appearance.fish
@@ -8,10 +8,10 @@ function set_dark_appearance
         --preview-window=wrap
         --marker='*'
         --color=fg:#908caa,bg:#232136,hl:#ea9a97
-	    --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
-	    --color=border:#44415a,header:#3e8fb0,gutter:#232136
-	    --color=spinner:#f6c177,info:#9ccfd8,separator:#44415a
-	    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
-	ln -s -f "/usr/local/opt/helix/libexec/runtime/themes/rose_pine_moon.toml" "$HOME/.config/helix/themes/rpine.toml"
+        --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+        --color=border:#44415a,header:#3e8fb0,gutter:#232136
+        --color=spinner:#f6c177,info:#9ccfd8,separator:#44415a
+        --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+    ln -s -f "/usr/local/opt/helix/libexec/runtime/themes/rose_pine_moon.toml" "$HOME/.config/helix/themes/rpine.toml"
     pkill -USR1 hx
 end
